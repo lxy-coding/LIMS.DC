73f72d389e272c64b87e6906dd94e45103de62e2
lxy <505691549@qq.com>
2021-02-20 09:03:27 +0800

    注释

--- LIMS.DC.Service/MainProcess.cs
+++ LIMS.DC.Service/MainProcess.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LIMS.DC.Service
@@ -12,21 +13,28 @@ namespace LIMS.DC.Service
         public static void Request()
         {
             DAManager daManager = new DAManager();
-            if( daManager.Request())
+
+            //连接不成功，延迟10s后
+            while( !daManager.Request())
             {
-                TaskHelper.Init();
-
-                RealObjectManager realObjectManager = new RealObjectManager();
-                realObjectManager.Request();
-
-                ActionManager actionManager = new ActionManager();
-                actionManager.Operator = daManager;
-                actionManager.Request();
-
-                CalManager calManager = new CalManager();
-                calManager.Operator = daManager;
-                calManager.Request();
+                Thread.Sleep(10000);
             }
+
+            TaskHelper.Init();
+
+            //实时数据
+            RealObjectManager realObjectManager = new RealObjectManager();
+            realObjectManager.Request();
+
+            //天车动作
+            ActionManager actionManager = new ActionManager();
+            actionManager.Operator = daManager;
+            actionManager.Request();
+
+            //校正时间
+            CalManager calManager = new CalManager();
+            calManager.Operator = daManager;
+            calManager.Request();
         }
     }
 }
